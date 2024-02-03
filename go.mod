8a0aa62de7bd73c8dd9669a233e134952e248632
goprojs <ayradas37@gmail.com>
2024-02-03 15:29:22 +0530

    Added code to check battery status

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/goprojs/power_status
+
+go 1.20
+
+require github.com/distatus/battery v0.11.0
+
+require (
+	golang.org/x/sys v0.9.0 // indirect
+	howett.net/plist v1.0.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/distatus/battery"
+)
+
+func batteryHasPowerSupply() (bool, error) {
+	batteries, err := battery.GetAll()
+	if err != nil {
+		return false, fmt.Errorf("error getting battery info: %v", err)
+	}
+	for i, battery := range batteries {
+		bState := battery.State.String()
+		fmt.Printf("Bat: %d has state: %s\n", i, bState)
+		if strings.Contains(bState, "Full") {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func main() {
+	hasPower, err := batteryHasPowerSupply()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(hasPower)
+}
