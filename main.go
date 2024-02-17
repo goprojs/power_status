4ed3fe39cf20d2deadac9d005a1b534490e58e83
Arabinda Das <7arabinda@gmail.com>
2024-02-17 11:53:17 +0530

    adding utils and odifying main.go

--- main.go
+++ main.go
@@ -1,125 +1,31 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"os"
-	"strings"
-	"time"
-
-	"github.com/distatus/battery"
+    "fmt"
+    "time"
+    "github.com/goprojs/power_status/utils"
 )
 
-type Config struct {
-	LocationID   string `json:"location_id"`
-	LocationName string `json:"location_name"`
-	ServerURL    string `json:"server_url"`
-}
-
-type indicator struct {
-	ElectricityStatus bool   `json:"estatus"`
-	LocationName      string `json:"location_name"`
-	LocationID        string `json:"location_id"`
-	CurrentTime       string `json:"timestamp"`
-}
-
-func batteryHasPowerSupply() (bool, error) {
-	batteries, err := battery.GetAll()
-	if err != nil {
-		return false, fmt.Errorf("error getting battery info: %v", err)
-	}
-	for i, battery := range batteries {
-		bState := battery.State.String()
-		fmt.Printf("Bat: %d has state: %s\n", i, bState)
-		if strings.Contains(bState, "Full") || strings.Contains(bState, "Charging") {
-			return true, nil
-		}
-	}
-	return false, nil
-}
-
-func sendDataToServer(data indicator, serverURL string) error {
-	//struct to json
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		return fmt.Errorf("error marshaling data: %v", err)
-	}
-
-	resp, err := http.Post(serverURL, "application/json", bytes.NewBuffer(jsonData))
-	if err != nil {
-		return fmt.Errorf("error posting data to server: %v", err)
-	}
-	defer resp.Body.Close()
-
-	//checking response status
-	statusCode := resp.StatusCode
-	fmt.Println(statusCode)
-	if (statusCode != 200) && (statusCode != 201) {
-		return fmt.Errorf("unexpected response status: %s", resp.Status)
-	}
-	return nil
-}
-
-func getAndSend() (bool, error) {
-	// Read config file
-	configFileName := "config.json"
-	configFile, err := os.ReadFile(configFileName)
-	if err != nil {
-		fmt.Println(err)
-		return false, err
-	}
-	var config Config
-	err = json.Unmarshal(configFile, &config)
-	if err != nil {
-		fmt.Println(err)
-		return false, err
-	}
-
-	currentTime := time.Now()
-	timeString := currentTime.Format("2006-01-02 15:04:05")
-
-	currentState, _ := batteryHasPowerSupply()
-	indicatorData := indicator{
-		ElectricityStatus: currentState,
-		LocationName:      config.LocationName,
-		LocationID:        config.LocationID,
-		CurrentTime:       timeString,
-	}
-
-	serverURL := config.ServerURL
-	err = sendDataToServer(indicatorData, serverURL)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return currentState, nil
-}
-
 func main() {
-	initialState, err := getAndSend()
-	// Send initial status to server
-	if err != nil {
-		fmt.Println("Error sending initial data:", err)
-	}
-	// Print initial battery status
-	fmt.Println("Initial battery status:", initialState)
-	for {
-		currentState, err := batteryHasPowerSupply()
-		if err != nil {
-			fmt.Println(err)
-		}
-		//fmt.Println(hasPower)
-		if currentState != initialState {
-			initialState = currentState
-			_, err := getAndSend()
-			if err != nil {
-				fmt.Println("Error sending initial data:", err)
-			}
-			time.Sleep(1 * time.Second)
-		}
-
-		// fmt.Println("not updating on 100% battery")
-
-	}
+    initialState, err := utils.GetAndSend("config.json")
+    // Send initial status to server
+    if err != nil {
+        fmt.Println("Error sending initial data:", err)
+    }
+    // Print initial battery status
+    fmt.Println("Initial battery status:", initialState)
+    for {
+        currentState, err := utils.BatteryHasPowerSupply()
+        if err != nil {
+            fmt.Println(err)
+        }
+        if currentState != initialState {
+            initialState = currentState
+            _, err := utils.GetAndSend("config.json")
+            if err != nil {
+                fmt.Println("Error sending initial data:", err)
+            }
+            time.Sleep(1 * time.Second)
+        }
+    }
 }
---
+++ utils/battery.go
@@ -0,0 +1,97 @@
+// power_status/utils/battery.go
+package utils
+
+import (
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "os"
+    "strings"
+    "time"
+
+    "github.com/distatus/battery"
+)
+
+type Config struct {
+    LocationID   string `json:"location_id"`
+    LocationName string `json:"location_name"`
+    ServerURL    string `json:"server_url"`
+}
+
+type Indicator struct {
+    ElectricityStatus bool   `json:"estatus"`
+    LocationName      string `json:"location_name"`
+    LocationID        string `json:"location_id"`
+    CurrentTime       string `json:"timestamp"`
+}
+
+func BatteryHasPowerSupply() (bool, error) {
+    batteries, err := battery.GetAll()
+    if err != nil {
+        return false, fmt.Errorf("error getting battery info: %v", err)
+    }
+    for i, battery := range batteries {
+        bState := battery.State.String()
+        fmt.Printf("Bat: %d has state: %s\n", i, bState)
+        if strings.Contains(bState, "Full") || strings.Contains(bState, "Charging") {
+            return true, nil
+        }
+    }
+    return false, nil
+}
+
+func SendDataToServer(data Indicator, serverURL string) error {
+    //struct to json
+    jsonData, err := json.Marshal(data)
+    if err != nil {
+        return fmt.Errorf("error marshaling data: %v", err)
+    }
+
+    resp, err := http.Post(serverURL, "application/json", bytes.NewBuffer(jsonData))
+    if err != nil {
+        return fmt.Errorf("error posting data to server: %v", err)
+    }
+    defer resp.Body.Close()
+
+    //checking response status
+    statusCode := resp.StatusCode
+    fmt.Println(statusCode)
+    if statusCode != 200 && statusCode != 201 {
+        return fmt.Errorf("unexpected response status: %s", resp.Status)
+    }
+    return nil
+}
+
+func GetAndSend(configFileName string) (bool, error) {
+    // Read config file
+    configFile, err := os.ReadFile(configFileName)
+    if err != nil {
+        fmt.Println(err)
+        return false, err
+    }
+    var config Config
+    err = json.Unmarshal(configFile, &config)
+    if err != nil {
+        fmt.Println(err)
+        return false, err
+    }
+
+    currentTime := time.Now()
+    timeString := currentTime.Format("2006-01-02 15:04:05")
+
+    currentState, _ := BatteryHasPowerSupply()
+    indicatorData := Indicator{
+        ElectricityStatus: currentState,
+        LocationName:      config.LocationName,
+        LocationID:        config.LocationID,
+        CurrentTime:       timeString,
+    }
+
+    serverURL := config.ServerURL
+    err = SendDataToServer(indicatorData, serverURL)
+    if err != nil {
+        fmt.Println(err)
+    }
+    return currentState, nil
+}
